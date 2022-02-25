274f289f6d38775c7719c2dd49842203a12ea376
sebastianignaciom <sebastianignaciom@hotmail.com>
2022-02-25 01:35:49 -0300

    Creación del componente acerca_de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './components/mi-componente/mi-componente.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiComponenteComponent,
-    EncabezadoComponent
+    EncabezadoComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule
