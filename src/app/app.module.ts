b380f16440a5c1bb030992a663eb9e3d4740b0f8
sebastianignaciom <sebastianignaciom@hotmail.com>
2022-02-25 01:27:12 -0300

    Creacion del componente encabezado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './components/mi-componente/mi-componente.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponenteComponent
+    MiComponenteComponent,
+    EncabezadoComponent
   ],
   imports: [
     BrowserModule
