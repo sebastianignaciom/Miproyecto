b7cda393b8749f57f53cdc47d6dce01cf903fedd
Sebastian <sebastianignaciomar@gmail.com>
2022-02-25 01:13:28 -0300

    Carga de plantilla HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MiComponenteComponent } from './components/mi-componente/mi-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponenteComponent
   ],
   imports: [
     BrowserModule
